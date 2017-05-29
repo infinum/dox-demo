5b19f6ef4ace99d13efcbc19b24b4817b082c6b4
Melita Kokot <melita.kokot@gmail.com>
2017-05-29 19:05:38 +0200

    Memoize request body deserialization

--- app/controllers/concerns/json_api_standard.rb
+++ app/controllers/concerns/json_api_standard.rb
@@ -2,7 +2,7 @@ module JsonApiStandard
   extend ActiveSupport::Concern
 
   def deserialized_params
-    ActionController::Parameters.new(
+    @deserialized_body ||= ActionController::Parameters.new(
       JSONAPI::Deserializable::Resource.call(JSON.parse(request.body.read))
     )
   end
