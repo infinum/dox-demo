db65685acc62d63d239c03be94fcc88bc16584b3
Melita Kokot <melita.kokot@gmail.com>
2017-05-29 11:02:08 +0200

    Update dox headers config

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -77,4 +77,5 @@ end
 Dox.configure do |config|
   config.header_file_path = Rails.root.join('spec/api_doc/v1/descriptions/header.md')
   config.desc_folder_path = Rails.root.join('spec/api_doc/v1/descriptions')
+  config.headers_whitelist = ['Accept']
 end
